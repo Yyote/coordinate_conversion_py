4e232769d74d59f548864d2c743d69fc800f7055
Andrey <yyootttaa@gmail.com>
2024-05-16 19:54:09 +0300

    Fixed northing and zone number being in the same request

--- coordinate_conversion_py/node.py
+++ coordinate_conversion_py/node.py
@@ -1,5 +1,6 @@
 import utm
 import rclpy
+import rclpy.logging
 from rclpy.node import Node
 
 from privyaznik_msgs.srv import UtmToWgs, WgsToUtm
@@ -13,14 +14,14 @@ class CoordinateTransferService(Node):
         self.wgs_to_utm_srv = self.create_service(WgsToUtm, 'wgs_to_utm', self.wgs_to_utm_cb)
 
     def utm_to_wgs_cb(self, request: UtmToWgs.Request, response: UtmToWgs.Response):
-        response.latitude, response.longitude = utm.to_latlon(request.easting, request.northing, request.zone_number, request.zone_letter, request.northern, True)
+        response.latitude, response.longitude = utm.to_latlon(easting=request.easting, northing=request.northing, zone_number=request.zone_number, zone_letter=request.zone_letter, strict=True)
 
         return response
 
     def wgs_to_utm_cb(self, request: WgsToUtm.Request, response: WgsToUtm.Response):
         # response.latitude, response.longitude = utm.to_latlon(request.easting, request.northing, request.zone_number, request.zone_letter, request.northern, True)
         response.easting, response.northing, response.zone_number, response.zone_letter = utm.from_latlon(request.latitude, request.longitude)
-        print("AAA")
+        self.get_logger().info('AAA')
         print(response)
         return response
 
